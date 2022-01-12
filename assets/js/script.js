0d092e2eb3d4ae517bb5ec46626764c6b9be1278
AdamPillis <adam.pillis@hotmail.com>
2022-01-12 14:14:05 +0000

    Add: perform get and post api requests.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,48 @@ const API_KEY = 'DJnnk6HvE5THhskryKoWi4zm3YM';
 const API_URL = 'https://ci-jshint.herokuapp.com/api';
 const resultsModal = new bootstrap.Modal(document.getElementById('resultsModal'));
 
-document.getElementById('status').addEventListener('click', e => getStatus(e));
+document.getElementById('status').addEventListener('click', event => getStatus(event));
+document.getElementById('submit').addEventListener('click', event => postForm(event));
 
-async function getStatus(e) {
+async function postForm(event) {
+    const form = new FormData(document.getElementById('checksform'));
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY,
+        },
+        body: form, 
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        displayErrors(data);
+    } else {
+        throw new Error(data.error);
+    }
+}
+
+function displayErrors(data) {
+    let heading = `JSHint Results for ${data.file}`;
+
+    if (data.total_errors === 0) {
+        results = `<div class='no_errors'>No errors reported!</div>`;
+    } else {
+        results = `<div>Total Errors: <span class='error_count'>${data.total_errors}</span></div>`;
+        for (let error of data.error_list) {
+            results +=  `<div>At the line <span class='line'>${error.line}</span>, `;
+            results += `column <span class='column'>${error.col}</span></div>`;
+            results += `<div class='error'>${error.error}</div>`;
+        }
+    }
+    document.getElementById('resultsModalTitle').innerText = heading;
+    document.getElementById('results-content').innerHTML = results;
+    resultsModal.show();
+}
+
+async function getStatus(event) {
     const queryString = `${API_URL}?api_key=${API_KEY}`;
 
     const response = await fetch(queryString);
@@ -28,3 +67,4 @@ function displayStatus(data) {
 
     resultsModal.show();
 }
+
